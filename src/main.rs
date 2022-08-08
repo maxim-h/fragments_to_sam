8110b61708d1e6a1d748006504d5caaf1eafb2a5
maxim-h <maksim.holmatov@gmail.com>
2022-08-08 15:48:50 +0200

    Handle different modes

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,143 @@ use std::io::{BufRead, Error, Stdout, Write};
 use std::path::Path;
 use noodles_bgzf as bgzf;
 use noodles_sam::{self as sam, header::{Program, ReferenceSequence, reference_sequence}, Header};
-use clap::Parser;
+use clap::{self, Parser, arg_enum};
 use noodles_sam::header::header::{GroupOrder, SortOrder, Version};
+
+
+struct PE {}
+struct F {}
+struct R {}
+
+arg_enum!{
+    #[derive(Debug, Clone)]
+    pub enum Mode {
+        PE,
+        F,
+        R,
+    }
+}
 
 
 /// Convert 10x style fragments file to SAM stream
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
+    /// Fragments file
     #[clap(short, long, value_parser)]
     fragments: String,
 
-    /// Number of times to greet
+    /// Chromosome sizes file
     #[clap(short, long, value_parser)]
     genome: String,
+
+    /// Output mode: PE, F, R
+    #[clap(short, long, value_parser)]
+    mode: Mode,
 }
 
+
+trait ParseAndSend {
+    fn parse_and_send(line: & mut String, writer: & mut io::BufWriter<Stdout>, header: &Header);
+}
+
+// this should be a macro
+impl ParseAndSend for PE {
+    fn parse_and_send(line: & mut String, writer: & mut io::BufWriter<Stdout>, header: &Header) {
+        let mut l = line.split("\t");
+
+        let chr = l.next().expect("Couldn't read ref name");
+        let start: usize = l.next().expect("Couldn't read start position")
+            .parse()
+            .expect("Couldn't parse start position")
+            ;
+        let end: usize = l.next()
+            .expect("Couldn't read end position")
+            .parse()
+            .expect("Couldn't parse end position")
+            ;
+        let length: usize = end - start;
+        let rn = l.next().expect("couldn't read the CB");
+
+        if !header.reference_sequences().contains_key(chr) {
+            return
+        }
+
+        writeln!(
+            writer,
+            "{}",
+            format!("{}\t67\t{}\t{}\t255\t{}M\t=\t{}\t0\t*\t*", rn, chr, start+1, length, start+1)
+        )
+            .expect("Couldn't send line");
+        writeln!(
+            writer,
+            "{}",
+            format!("{}\t147\t{}\t{}\t255\t{}M\t=\t{}\t0\t*\t*", rn, chr, start+1, length, start+1)
+        )
+            .expect("Couldn't send line");
+    }
+}
+
+impl ParseAndSend for F {
+    fn parse_and_send(line: & mut String, writer: & mut io::BufWriter<Stdout>, header: &Header) {
+        let mut l = line.split("\t");
+
+        let chr = l.next().expect("Couldn't read ref name");
+        let start: usize = l.next().expect("Couldn't read start position")
+            .parse()
+            .expect("Couldn't parse start position")
+            ;
+        let end: usize = l.next()
+            .expect("Couldn't read end position")
+            .parse()
+            .expect("Couldn't parse end position")
+            ;
+        let length: usize = end - start;
+        let rn = l.next().expect("couldn't read the CB");
+
+        if !header.reference_sequences().contains_key(chr) {
+            return
+        }
+
+        writeln!(
+            writer,
+            "{}",
+            format!("{}\t67\t{}\t{}\t255\t{}M\t=\t{}\t0\t*\t*", rn, chr, start+1, length, start+1)
+        )
+            .expect("Couldn't send line");
+    }
+}
+
+
+impl ParseAndSend for R {
+    fn parse_and_send(line: & mut String, writer: & mut io::BufWriter<Stdout>, header: &Header) {
+        let mut l = line.split("\t");
+
+        let chr = l.next().expect("Couldn't read ref name");
+        let start: usize = l.next().expect("Couldn't read start position")
+            .parse()
+            .expect("Couldn't parse start position")
+            ;
+        let end: usize = l.next()
+            .expect("Couldn't read end position")
+            .parse()
+            .expect("Couldn't parse end position")
+            ;
+        let length: usize = end - start;
+        let rn = l.next().expect("couldn't read the CB");
+
+        if !header.reference_sequences().contains_key(chr) {
+            return
+        }
+
+        writeln!(
+            writer,
+            "{}",
+            format!("{}\t147\t{}\t{}\t255\t{}M\t=\t{}\t0\t*\t*", rn, chr, start+1, length, start+1)
+        )
+            .expect("Couldn't send line");
+    }
+}
 
 fn read_genome(genome: &Path) -> Vec<(reference_sequence::Name, usize)> {
     let f = std::fs::File::open(genome).expect("Couldn't open genome file");
@@ -46,42 +166,7 @@ fn read_genome(genome: &Path) -> Vec<(reference_sequence::Name, usize)> {
     res
 }
 
-fn parse_and_send(line: & mut String, writer: & mut io::BufWriter<Stdout>, header: &Header) {
-    let mut l = line.split("\t");
 
-    let chr = l.next().expect("Couldn't read ref name");
-    let start: usize = l.next().expect("Couldn't read start position")
-        .parse()
-        .expect("Couldn't parse start position")
-        ;
-    let end: usize = l.next()
-        .expect("Couldn't read end position")
-        .parse()
-        .expect("Couldn't parse end position")
-        ;
-    let length: usize = end - start;
-    let rn = l.next().expect("couldn't read the CB");
-
-    if !header.reference_sequences().contains_key(chr) {
-        return
-    }
-
-    // let x = format!("{}\t67\t{}\t{}\t255\t{}M\t=\t{}\t0\t*\t*", rn, chr, start+1, length, start+1);
-    // writer.write(x.as_bytes());
-    // writer.write(format!("{}\t67\t{}\t{}\t255\t{}M\t=\t{}\t0\t*\t*", rn, chr, start+1, length, start+1));
-    writeln!(
-        writer,
-        "{}",
-        format!("{}\t67\t{}\t{}\t255\t{}M\t=\t{}\t0\t*\t*", rn, chr, start+1, length, start+1)
-    )
-        .expect("asd");
-    writeln!(
-        writer,
-        "{}",
-        format!("{}\t147\t{}\t{}\t255\t{}M\t=\t{}\t0\t*\t*", rn, chr, start+1, length, start+1)
-    )
-        .expect("asd");
-}
 
 
 
@@ -112,7 +197,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .build()
         )
         .set_reference_sequences(reference_sequences)
-        .add_program(Program::new("bed_to_bam"))
+        .add_program(Program::new("fragments_to_sam"))
         .add_comment("SAM output made with noodles")
         .build();
 
@@ -127,9 +212,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let lines = reader.lines();
 
     let mut writer= io::BufWriter::new(io::stdout());
-    for line in lines {
-        parse_and_send(& mut line.expect("Didn't receive a line"), & mut writer, &header);
+
+    match args.mode {
+        Mode::PE => {
+            for line in lines {
+                PE::parse_and_send(& mut line.expect("Didn't receive a line"), & mut writer, &header);
+            }
+        },
+        Mode::F => {
+            for line in lines {
+                F::parse_and_send(& mut line.expect("Didn't receive a line"), & mut writer, &header);
+            }
+        },
+        Mode::R => {
+            for line in lines {
+                R::parse_and_send(& mut line.expect("Didn't receive a line"), & mut writer, &header);
+            }
+        },
     }
+
 
     Ok(())
 }
